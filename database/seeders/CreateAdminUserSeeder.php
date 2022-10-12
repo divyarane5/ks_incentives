981def3c0dec12dcfa12b195c524d9ce0e18d9f7
Vrushali <vrushali.bangar@karvyinfotech.com>
2022-10-12 14:42:58 +0530

    created seeder

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'email' => 'admin@homebazaar.com',
+            'password' => bcrypt('123456'),
+            'employee_code' => 'VSH_OP_001',
+            'entity' => 'Homebazaar',
+            'first_name' => 'Homebazaar',
+            'last_name' => 'Admin',
+            'created_by' => 1
+        ]);
+
+        $role = Role::create(['name' => 'Superadmin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'location-view',
+            'location-create',
+            'location-edit',
+            'location-delete',
+            'department-view',
+            'department-create',
+            'department-edit',
+            'department-delete',
+            'designation-view',
+            'designation-create',
+            'designation-edit',
+            'designation-delete',
+            'user-view',
+            'user-create',
+            'user-edit',
+            'user-delete',
+            'expense-view',
+            'expense-create',
+            'expense-edit',
+            'expense-delete',
+            'vendor-view',
+            'vendor-create',
+            'vendor-edit',
+            'vendor-delete',
+            'business_unit-view',
+            'business_unit-create',
+            'business_unit-edit',
+            'business_unit-delete',
+            'payment_method-view',
+            'payment_method-create',
+            'payment_method-edit',
+            'payment_method-delete',
+            'configuration-view',
+            'configuration-create',
+            'configuration-edit',
+            'configuration-delete',
+            'role-view',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'role-permission',
+            'project-view',
+            'developer-view',
+            'intend-view-all',
+            'intend-view-own',
+            'intend-create',
+            'intend-edit',
+            'intend-delete',
+            'intend-approve-log',
+            'intend-payment-view',
+            'intend-payment-create',
+            'intend-payment-edit',
+            'intend-payment-delete',
+            'intend-payment-conclude',
+            'reimbursement-view',
+            'reimbursement-create',
+            'reimbursement-edit',
+            'reimbursement-delete',
+            'reimbursement-approve-log',
+            'referral-template-view',
+            'referral-template-create',
+            'referral-template-edit',
+            'referral-template-delete',
+            'referral-client-view',
+            'referral-client-create',
+            'referral-client-edit',
+            'referral-client-delete',
+            'referral-client-send-email',
+            'referral-service-view',
+            'project-offer-view',
+            'project-offer-create',
+            'project-offer-edit',
+            'project-offer-send-image-email',
+            'project-offer-send-pdf-email',
+         ];
+
+         foreach ($permissions as $permission) {
+              Permission::create(['name' => $permission]);
+         }
+    }
+}
