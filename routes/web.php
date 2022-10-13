6413021158218496895cef68543192709b849d8e
divya <divya.rane@karvyinfotech.com>
2022-10-13 13:43:53 +0530

    started user module

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
     Route::get('/account', [App\Http\Controllers\DashboardController::class, 'account'])->name('account');
+    Route::resource('users', App\Http\Controllers\UserController::class);
 });
 
 
