e4ef8d5ef0abc1cf4585f65b399c1b544470933b
Vrushali <vrushali.bangar@karvyinfotech.com>
2022-11-18 13:59:58 +0530

    changes

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -8,7 +8,7 @@ class ClientRepository implements ClientRepositoryInterface
 {
     public function getClientDetails($id)
     {
-        Client::select('users.name as u_name','users.id as u_id','designations.name as d_name','clients.client_name','clients.client_email','clients.sales_person','clients.created_by','templates.content','clients.id','users.mobile','clients.subject_name')
+        return Client::select('users.name as u_name','users.id as u_id','designations.name as d_name','clients.client_name','clients.client_email','clients.sales_person','clients.created_by','templates.content','clients.id','users.mobile','clients.subject_name')
                     ->join('templates', 'clients.template_id','=','templates.id')
                     ->join('users', 'clients.created_by','=','users.id')
                     ->leftJoin('designations', 'users.designation_id','=','designations.id','inner')
